5a7d35fda1a2924b57a59387c625785e784d537b
vekusxd <batmanovemil@gmail.com>
2025-10-19 16:17:42 +0300

    Feature/2 (#9)
    * feature: сделал базовую реализацию регистрации
    
    * Добавил GlobalExceptionHandler
    
    * Добавил миграцию
    
    * Добавил ValidationBehavior
    
    * Добавилл endpoint для регистрации
    
    * поменял статус ответа на 200

---
+++ src/DiaryAssistance.Application/Auth/Register/RegisterCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace DiaryAssistance.Application.Auth.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(c => c.FirstName).NotNull().NotEmpty();
+        RuleFor(c => c.LastName).NotNull().NotEmpty();
+        RuleFor(c => c.Email).NotNull().NotEmpty().EmailAddress();
+        RuleFor(c => c.Password).NotNull().NotEmpty();
+        RuleFor(c => c.Group).NotNull().NotEmpty();
+    }
+}
