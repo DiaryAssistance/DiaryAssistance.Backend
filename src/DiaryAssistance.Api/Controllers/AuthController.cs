1d79a29ab9542bcc02275dee264a1745ec6fd566
Emil Batmanov <batmanovemil@gmail.com>
2025-10-19 15:32:53 +0300

    Добавилл endpoint для регистрации

--- src/DiaryAssistance.Api/Controllers/AuthController.cs
+++ src/DiaryAssistance.Api/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using DiaryAssistance.Application.Auth.Login;
+using DiaryAssistance.Application.Auth.Models;
+using DiaryAssistance.Application.Auth.Register;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DiaryAssistance.Api.Controllers;
 
@@ -8,6 +11,16 @@ public class AuthController : BaseController
 {
     [HttpPost("/login")]
     public async Task<IActionResult> Login(LoginCommand request, CancellationToken cancellationToken)
+    {
+        return Ok(await Mediator.Send(request, cancellationToken));
+    }
+
+    [HttpPost("/register")]
+    [ProducesResponseType<UserResponse>(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> Register([FromBody] RegisterCommand request, CancellationToken cancellationToken)
     {
         return Ok(await Mediator.Send(request, cancellationToken));
     }
