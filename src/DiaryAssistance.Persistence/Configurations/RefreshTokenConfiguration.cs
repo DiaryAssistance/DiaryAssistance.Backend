f7ac963253d46d327172aa2a6e862f3cbcaeea94
Emil Batmanov <batmanovemil@gmail.com>
2025-10-19 16:57:53 +0300

    Добавил Expires в RefreshToken

---
+++ src/DiaryAssistance.Persistence/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using DiaryAssistance.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DiaryAssistance.Persistence.Configurations;
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(x => x.Id);
+        
+        builder
+            .HasOne(r => r.User)
+            .WithMany(u => u.RefreshTokens)
+            .HasForeignKey(r => r.UserId);
+        
+        builder
+            .Property(x => x.Token)
+            .IsRequired()
+            .HasMaxLength(128);
+
+        builder
+            .Property(x => x.Expires)
+            .HasColumnName("Expires")
+            .IsRequired();
+    }
+}
---
+++ src/DiaryAssistance.Persistence/Migrations/20251019135123_CreatedAtRefreshToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiaryAssistance.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAtRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "RefreshTokens",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2025, 10, 19, 13, 51, 22, 884, DateTimeKind.Utc).AddTicks(3439));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "RefreshTokens");
+        }
+    }
+}
