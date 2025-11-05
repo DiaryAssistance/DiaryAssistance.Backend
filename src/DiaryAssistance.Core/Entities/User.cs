2c12695db962fbd62d8e5024bb295201dbd097b9
Emil Batmanov <batmanovemil@gmail.com>
2025-11-05 20:11:05 +0300

    Обновил группу у пользователя

--- src/DiaryAssistance.Core/Entities/User.cs
+++ src/DiaryAssistance.Core/Entities/User.cs
@@ -6,6 +6,9 @@ public class User : IdentityUser<Guid>
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Group { get; set; } = string.Empty;
+
+    public Guid? GroupId { get; set; }
+    public Group? Group { get; set; }
+
     public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
 }
